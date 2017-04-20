fff102bad298c96327320566b7c7f8aec9ce6e9f
preawbnp <budnampetch.o@ku.th>
2017-04-20 13:21:03 +0700

    intital empty app

---
+++ app/src/main/java/com/example/preawbnp/bookstore/listBooks.java
@@ -0,0 +1,13 @@
+package com.example.preawbnp.bookstore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class listBooks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_books);
+    }
+}
