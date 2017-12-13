44b70bd7fcfe60dabc43ab05cd9aa29601916e5a
preawbnp <preawbnp@preawbnps-MacBook-Pro.local>
2017-12-14 02:16:25 +0700

    create intital data for book informations

---
+++ app/src/main/java/com/example/preawbnp/bookstore/model/Book.java
@@ -0,0 +1,46 @@
+package com.example.preawbnp.bookstore.model;
+
+public class Book {
+    private int id;
+    private String title;
+    private double price;
+    private int year;
+    private String imageUrl;
+
+    public Book(int id, String title, double price, int year) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.year = year;
+        this.imageUrl = null;
+    }
+
+    public String toString() {
+        return title + " " + year + " (" + price + ")";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+}
